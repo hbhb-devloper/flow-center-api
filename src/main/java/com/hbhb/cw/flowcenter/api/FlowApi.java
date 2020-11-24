619d63e329872908c3715b06f5dad7ea85394d2a
wangxiaogang <15151269@163.com>
2020-11-24 11:42:23 +0800

    流程api

--- src/main/java/com/hbhb/cw/flowcenter/api/FlowApi.java
+++ src/main/java/com/hbhb/cw/flowcenter/api/FlowApi.java
@@ -5,20 +5,10 @@ import com.hbhb.cw.flowcenter.model.Flow;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
-
 /**
  * @author wangxiaogang
  */
 public interface FlowApi {
-    /**
-     * 跟据单位id角色id获取单位下某一角色对应用户
-     *
-     * @param roleName 单位id
-     * @return 用户id
-     */
-    @GetMapping("/role-list")
-    List<Integer> getFlowRoleUserList(@RequestParam("roleName") String roleName);
 
     /**
      * 跟据流程id获取流程信息
--- src/main/java/com/hbhb/cw/flowcenter/api/FlowRoleUserApi.java
+++ src/main/java/com/hbhb/cw/flowcenter/api/FlowRoleUserApi.java
@@ -1,6 +1,7 @@
 package com.hbhb.cw.flowcenter.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -8,6 +9,14 @@ import java.util.List;
  * @author wangxiaogang
  */
 public interface FlowRoleUserApi {
+    /**
+     * 跟据单位id角色id获取单位下某一角色对应用户
+     *
+     * @param roleName 单位id
+     * @return 用户id
+     */
+    @GetMapping("/role-list")
+    List<Integer> getFlowRoleUserList(@RequestParam("roleName") String roleName);
 
     /**
      * 通过用户id得到流程角色
