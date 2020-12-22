998fc32c6d55f5df365f2dba157c89de6df535d5
wangxiaogang <15151269@163.com>
2020-12-22 19:32:08 +0800

    角色列表

--- src/main/java/com/hbhb/cw/flowcenter/api/FlowRoleUserApi.java
+++ src/main/java/com/hbhb/cw/flowcenter/api/FlowRoleUserApi.java
@@ -1,5 +1,7 @@
 package com.hbhb.cw.flowcenter.api;
 
+
+import com.hbhb.api.core.bean.SelectVO;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -11,7 +13,7 @@ import java.util.List;
 public interface FlowRoleUserApi {
 
     @GetMapping("/user-id")
-    List<Integer> getUserIdByRoleName(@RequestParam("roleName") String roleName);
+    List<SelectVO> getUserIdByRoleName(@RequestParam("roleName") String roleName);
 
     @GetMapping("/role-name")
     List<String> getRoleNameByUserId(@RequestParam("userId") Integer userId);
